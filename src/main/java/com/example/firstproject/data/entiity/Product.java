079ce770eac9665d6f59f9d6cb88bc0de1b6ba34
seungryeol <opop0421@naver.com>
2023-07-01 13:16:42 +0900

    Mysql 연동 후 엔티티 설계

---
+++ src/main/java/com/example/firstproject/data/entiity/Product.java
@@ -0,0 +1,30 @@
+package com.example.firstproject.data.entiity;
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createAt;
+    private  LocalDateTime updateAt;
+
+
+
+}
