aa8be1289fb00887c9607071316736910d089124
seungryeol <opop0421@naver.com>
2023-07-05 18:03:19 +0900

    레포지토리 테스트 코드작성

--- src/main/java/com/example/firstproject/data/repository/ProductRepository.java
+++ src/main/java/com/example/firstproject/data/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.example.firstproject.data.repository;
 
 import com.example.firstproject.data.entiity.Product;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
