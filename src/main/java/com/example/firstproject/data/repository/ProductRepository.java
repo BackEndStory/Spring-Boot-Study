6363009ff095d15cadc8b6cede56ffcf69c6d5bd
seungryeol <opop0421@naver.com>
2023-07-06 12:40:15 +0900

    SpringBoot Jpa 조회 정렬함수

--- src/main/java/com/example/firstproject/data/repository/ProductRepository.java
+++ src/main/java/com/example/firstproject/data/repository/ProductRepository.java
@@ -6,6 +6,83 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    Optional<Product> findByNumber(Long number);
+    List<Product> findAllBy(String name);
+    Product queryByNumber(Long number);
+
+    boolean existsByNumber(Long number);
+
+    long countByName(String name);                          // 조회 쿼리 개수
+
+    void deleteByNumber(Long number);
+
+    long removeByName(String name);
+
+    List<Product> findFirst5ByName(String name);
+
+    List<Product> findTop10ByName(String name);
+
+    Product findByNumberIs(Long number);
+
+    Product findByNumberEquals(Long number);
+
+    Product findByNumberIsNot(Long number);
+
+    Product findByNumberNot(Long number);
+
+    List<Product> findByUpdatedAtNull();
+
+    Product findByNumberAndName(Long number, String name);
+
+    Product findByNumberOrName(Long number, String name);
+
+    List<Product> findByPriceGreaterThan(Long price);
+
+    List<Product> findByPriceGreaterThanEqual(Long price);
+
+    List<Product> findByPriceLessThanEqual(Long price);
+
+    List<Product> findByPriceLessThan(Long price);
+
+    List<Product> findByPriceBetween(Long price, Long highPrice);
+
+
+    List<Product> findByNameLike(String name);
+
+    List<Product> findByNameContains(String name);
+    List<Product> findByNameContaining(String name);
+
+
+    List<Product> findByNameStartsWith(String name);
+    List<Product> findByNameStartingWith(String name);
+
+
+    List<Product> findByNameEndsWith(String name);
+    List<Product> findByNameEndingWith(String name);
+
+    List<Product> findByNameOrderByNumberAsc(String name);
+
+    List<Product> findByNameOrderByNumberDesc(String name);
+
+    List<Product> findByNameOrderByPriceAscStockDesc(String name);
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ src/test/java/com/example/firstproject/data/repository/ProductRepositoryTest3.java
@@ -0,0 +1,51 @@
+package com.example.firstproject.data.repository;
+
+
+import com.example.firstproject.data.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.example.firstproject.data.entiity.Product;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+class ProductRepositoryTest3 {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void sortingAndPagingTest() {
+        // given
+        Product product1 = new Product();
+        product1.setName("펜");
+        product1.setPrice(1000);
+        product1.setStock(100);
+        product1.setCreatedAt(LocalDateTime.now());
+        product1.setUpdatedAt(LocalDateTime.now());
+
+        Product product2 = new Product();
+        product2.setName("펜");
+        product2.setPrice(5000);
+        product2.setStock(300);
+        product2.setCreatedAt(LocalDateTime.now());
+        product2.setUpdatedAt(LocalDateTime.now());
+
+        Product product3 = new Product();
+        product3.setName("펜");
+        product3.setPrice(500);
+        product3.setStock(50);
+        product3.setCreatedAt(LocalDateTime.now());
+        product3.setUpdatedAt(LocalDateTime.now());
+
+        Product savedProduct1 = productRepository.save(product1);
+        Product savedProduct2 = productRepository.save(product2);
+        Product savedProduct3 = productRepository.save(product3);
+
+    }
+
+
+}
