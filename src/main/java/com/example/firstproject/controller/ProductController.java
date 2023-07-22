2cf74cd514bc39935eee2ae4f8ae1e02507c620b
seungryeol <opop0421@naver.com>
2023-07-22 14:13:03 +0900

    Security jwt 로그인 회원가입 물품 등록 최종 마무리

--- src/main/java/com/example/firstproject/controller/ProductController.java
+++ src/main/java/com/example/firstproject/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.example.firstproject.controller;
 import com.example.firstproject.data.dto.ProductDto;
 import com.example.firstproject.data.dto.ProductResponseDto;
 import com.example.firstproject.service.ProductService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,6 @@ import com.example.firstproject.data.dto.ChangeProductNameDto;
 public class ProductController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
-
     private final ProductService productService;
 
     @Autowired
@@ -25,36 +26,65 @@ public class ProductController {
 
     @GetMapping()
     public ResponseEntity<ProductResponseDto> getProduct(Long number) {
-
+        long currentTime = System.currentTimeMillis();
+        LOGGER.info("[getProduct] request Data :: productId : {}", number);
         ProductResponseDto productResponseDto = productService.getProduct(number);
 
+        LOGGER.info(
+                "[getProduct] response Data :: productId : {}, productName : {}, productPrice : {}, productStock : {}",
+                productResponseDto.getNumber(), productResponseDto.getName(),
+                productResponseDto.getPrice(), productResponseDto.getStock());
+        LOGGER.info("[getProduct] Response Time : {}ms", System.currentTimeMillis() - currentTime);
         return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
+    })
     @PostMapping()
     public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductDto productDto) {
-
+        long currentTime = System.currentTimeMillis();
         ProductResponseDto productResponseDto = productService.saveProduct(productDto);
 
+        LOGGER.info("[createProduct] Response Time : {}ms", System.currentTimeMillis() - currentTime);
         return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
+    })
     @PutMapping()
     public ResponseEntity<ProductResponseDto> changeProductName(
             @RequestBody ChangeProductNameDto changeProductNameDto) throws Exception {
+        long currentTime = System.currentTimeMillis();
+        LOGGER.info("[changeProductName] request Data :: productNumber : {}, productName : {}",
+                changeProductNameDto.getNumber(), changeProductNameDto.getName());
 
         ProductResponseDto productResponseDto = productService.changeProductName(
                 changeProductNameDto.getNumber(),
                 changeProductNameDto.getName());
 
+        LOGGER.info(
+                "[changeProductName] response Data :: productNumber : {}, productName : {}, productPrice : {}, productStock : {}",
+                productResponseDto.getNumber(), productResponseDto.getName(),
+                productResponseDto.getPrice(), productResponseDto.getStock());
+        LOGGER.info("[changeProductName] response Time : {}ms",
+                System.currentTimeMillis() - currentTime);
         return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
-
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
+    })
     @DeleteMapping()
     public ResponseEntity<String> deleteProduct(Long number) throws Exception {
+        long currentTime = System.currentTimeMillis();
+        LOGGER.info("[deleteProduct] request Data :: productNumber : {}", number);
+
         productService.deleteProduct(number);
 
+        LOGGER.info("[deleteProduct] response Time : {}ms",
+                System.currentTimeMillis() - currentTime);
         return ResponseEntity.status(HttpStatus.OK).body("정상적으로 삭제되었습니다.");
     }
 
